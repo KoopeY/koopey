2da575c59824baf809054a13f069718bc2bc9cb6
Hann1bal <apalchev@gmail.com>
2017-06-25 00:33:20 +0300

    Merge branches 'feature/camera' and 'map' of https://github.com/KoopeY/koopey into map
    # Conflicts:
    #	build.gradle

--- android/assets/map/map.tsx
+++ android/assets/map/map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tile22" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="../../../../../Desktop/tile22.png" width="16" height="16"/>
+<tileset name="map" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="gfx/Overworld.png" width="640" height="576"/>
 </tileset>
